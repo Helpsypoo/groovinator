7cf1240c84640a1eff6e4a03e3822aa3e612f24a
Nate Barrett <n8barrett@gmail.com>
2026-09-22 15:19:22 -0500

    SOUND TEST

--- Paint Element Scripts/whisper_lady.gd
+++ Paint Element Scripts/whisper_lady.gd
@@ -2,9 +2,9 @@ extends PickableAnimatedSprite3D
 
 func on_click() -> void:
 	var tween = create_tween()
-	tween.tween_property(self, "position", position + Vector3.DOWN/4, 0.4)
+	tween.tween_property(self, "position", position + Vector3.DOWN/15, 1)
 	play(&"default")
-	tween.tween_property(self, "position", position, 0.1)
+	tween.tween_property(self, "position", position, 0.2)
 
 
 
--- Paint Element Scripts/yellowguy_arm.gd
+++ Paint Element Scripts/yellowguy_arm.gd
@@ -1,5 +1,14 @@
 extends PickableAnimatedSprite3D
 
-func on_click() -> void:
-	play(&"default")
+@onready var audio_player = AudioStreamPlayer3D.new()
+
+func _ready() -> void:
+	add_child(audio_player)
+	audio_player.stream = preload("res://sounds/lobster.mp3")
 	
+func on_click() -> void:
+	var tween = create_tween()
+	tween.tween_property(self, "position", position + Vector3.DOWN/6, 1.5)
+	audio_player.play()
+	play(&"default")
+	tween.tween_property(self, "position", position, 0.3)
